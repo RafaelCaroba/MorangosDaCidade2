14dea746fe0cd4ba6e43e26551ae99f792795ee0
Nicole Ferraz <64486678+Nickbela@users.noreply.github.com>
2024-06-06 17:19:14 -0300

    Add files via upload

--- MorangosDaCidade2/repositories/ClienteRepository.cs
+++ MorangosDaCidade2/repositories/ClienteRepository.cs
@@ -1,6 +1,7 @@
 ﻿using MorangosDaCidade.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Data;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
@@ -52,21 +53,21 @@ namespace MorangosDaCidade.Repository
             return resultado;
         }
 
-        public List<Cliente> ListarClienteS()
+        public List<Cliente> ListarClientes()
         {
             using (SqlConnection connection = new SqlConnection(stringDeConexao))
             {
                 try
                 {
                     connection.Open();
-                    string query = "SELECT IdFunc, NOME, CPF, EMAIL, TELEFONE, DataNascimento FROM dbo.CLIENTE";
+                    string query = "SELECT IdCli, NOME, CPF, EMAIL, TELEFONE, DataNascimento FROM dbo.CLIENTE";
                     SqlCommand comando = new SqlCommand(query, connection);
                     List<Cliente> clientes = new List<Cliente>();
                     SqlDataReader reader = comando.ExecuteReader();
                     while (reader.Read())
                     {
                         Cliente f = new Cliente();
-                        f.Id = (int)reader["IdFunc"];
+                        f.Id = (int)reader["IdCli"];
                         f.Nome = (string)reader["NOME"];
                         f.Cpf = (string)reader["CPF"];
                         f.Email = (string)reader["EMAIL"];
@@ -89,6 +90,52 @@ namespace MorangosDaCidade.Repository
             return null;
         }
 
+        public bool LoginCliente()
+        {
+            using (SqlCommand comando = new SqlCommand(stringDeConexao))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT EMAIL, SENHA FROM dbo.CLIENTE" + "WHERE EMAIL LIKE @Email" + "WHERE SENHA LIKE @Senha";
+
+                    SqlCommand comando = new SqlCommand(query, connection);
+                    comando.Parameters.AddWithValue("@Email", $"%{email}%");
+
+                    //SqlDataReader reader = comando.ExecuteReader()
+
+                    using (SqlDataReader reader = comando.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            if (reader["senha"].ToString() == senha)
+                            {
+                                result = "logado com sucesso."
+                            }
+                            else
+                            {
+                                result = "Senha inválida!";
+                            }
+                        }
+                        else
+                        {
+                            result = "Usuário não encontrado!";
+                        }
+                    }
+                    //comando.Parameters.AddWithValue("@Senha", $"%{senha}%");
+;
+                    return true;
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Erro de SQL: " + ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro: " + ex.Message);
+                }
+            }
+        }
         public List<Cliente> BuscarClientePorNome(string nome)
         {
             using (SqlConnection connection = new SqlConnection(stringDeConexao))
@@ -96,7 +143,7 @@ namespace MorangosDaCidade.Repository
                 try
                 {
                     connection.Open();
-                    string query = "SELECT IdFunc, NOME, CPF, EMAIL, TELEFONE, DataNascimento FROM dbo.CLIENTE" +
+                    string query = "SELECT IdCli, NOME, CPF, EMAIL, TELEFONE, DataNascimento FROM dbo.CLIENTE" +
                         " WHERE NOME LIKE @Nome";
                     SqlCommand comando = new SqlCommand(query, connection);
                     comando.Parameters.AddWithValue("@Nome", $"%{nome}%");
@@ -134,15 +181,15 @@ namespace MorangosDaCidade.Repository
                 try
                 {
                     connection.Open();
-                    string query = "SELECT IdFunc, NOME, CPF, EMAIL, TELEFONE, DataNascimento FROM dbo.CLIENTE" +
-                        " WHERE IdFunc = @Id";
+                    string query = "SELECT IdCli, NOME, CPF, EMAIL, TELEFONE, DataNascimento FROM dbo.CLIENTE" +
+                        " WHERE IdCli = @Id";
                     SqlCommand comando = new SqlCommand(query, connection);
                     comando.Parameters.AddWithValue("@Id", id);
                     SqlDataReader reader = comando.ExecuteReader();
                     if (reader.Read())
                     {
                         Cliente f = new Cliente();
-                        f.Id = (int)reader["IdFunc"];
+                        f.Id = (int)reader["IdCli"];
                         f.Nome = (string)reader["NOME"];
                         f.Cpf = (string)reader["CPF"];
                         f.Email = (string)reader["EMAIL"];
@@ -170,7 +217,7 @@ namespace MorangosDaCidade.Repository
             Console.WriteLine(cliente.Id);
             int resultado = 0;
             string query = "UPDATE dbo.CLIENTE SET Nome = @NovoNome, CPF = @NovoCPF, " +
-                    "Email = @NovoEmail, DataNascimento = @NovaDataNascimento, Senha = @NovaSenha WHERE IdFunc = @Id";
+                    "Email = @NovoEmail, DataNascimento = @NovaDataNascimento, Senha = @NovaSenha WHERE IdCli = @Id";
 
             using (SqlConnection connection = new SqlConnection(stringDeConexao))
             {
@@ -208,7 +255,7 @@ namespace MorangosDaCidade.Repository
                 try
                 {
                     connection.Open();
-                    string query = "DELETE FROM dbo.CLIENTE WHERE IdFunc = @Id";
+                    string query = "DELETE FROM dbo.CLIENTE WHERE IdCli = @Id";
                     SqlCommand command = new SqlCommand(query, connection);
                     command.Parameters.AddWithValue("@Id", id);
                     resultado = command.ExecuteNonQuery();
